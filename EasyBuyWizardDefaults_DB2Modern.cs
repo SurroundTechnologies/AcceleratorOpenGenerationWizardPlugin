e1ae9d01b251e9557dd9d8c7650bf2e3f19ac1df
Lee Paul <lpaul@surroundtech.com>
2023-08-03 22:53:21 -0400

    Updated Example Plugin EasyBuyWizardDefaults_DB2Modern

--- EasyBuyWizardDefaults_DB2Modern.cs
+++ EasyBuyWizardDefaults_DB2Modern.cs
@@ -58,48 +58,19 @@ namespace GenerationWizardPlugin
 
 
                     // Set TimeStamp, TIMESTMP, Type fields to DateTime
-                    if ((viewColumnEntity.Type == "TIMESTMP(0)"))
+                    if (viewColumnEntity.Type.Contains("TIMESTMP"))
                     {
                         viewColumnEntity.PropertyType = AB_PropertyTypes.DateTime;
                         viewColumnEntity.FieldVisualization = AB_FieldVisualizations.AB_DateTimePickerWithLabel;
                     }
 
-                    // SetAdd Attribute [AB_AutoIncremented] to Identity Field
-                    if (IsIdentityField(viewColumnEntity))
-                    {
-                        viewColumnEntity.IsIdentity = false;
-                        if (viewColumnEntity.Type == "NUMERIC(8.0)")
-                        {
-                            viewColumnEntity.IsAutoIncrementedInCode = true;
-                        }
-                    }
-
-                    // Uncheck Memo Field from Content Window and Search Explorer Bar
-                    if (viewColumnEntity.ViewField.EndsWith("M1") && viewColumnEntity.FieldLength == 100)
-                    {
-                        viewColumnEntity.IsContentWindowField = false;
-                        viewColumnEntity.IsExplorerBarField = false;
-                    }
-
-                    // Uncheck Image Path from Search Explorer Bar
-                    if (viewColumnEntity.ViewField.EndsWith("IMPT") && viewColumnEntity.FieldLength == 256)
-                    {
-                        viewColumnEntity.IsExplorerBarField = false;
-                    }
-
                     // Uncheck Warehouse ID and Sales Person ID Fields from Content Window, Search Explorer Bar and Detail
-                    if (viewColumnEntity.ViewField.EndsWith("YD1O1WID") || viewColumnEntity.ViewField.EndsWith("YD1O1AID"))
+                    if (viewColumnEntity.ViewField == "WarhouseInternalID" || viewColumnEntity.ViewField == "SalesPersonInternalID")
                     {
                         viewColumnEntity.IsContentWindowField = false;
                         viewColumnEntity.IsExplorerBarField = false;
                         viewColumnEntity.IsDetailField = false;
                         viewColumnEntity.IsAuditStampField = false;
-                    }
-
-                    // Set Nick Name Field to Visible False in Content Window
-                    if (viewColumnEntity.ViewField.EndsWith("NN") && viewColumnEntity.FieldLength == 50)
-                    {
-                        viewColumnEntity.Visible = false;
                     }
 
                     // Set Fields in Title Required
@@ -112,20 +83,10 @@ namespace GenerationWizardPlugin
                         viewColumnEntity.IsRequiredField = false;
                     }
 
-                    // Set Any Fields that are not audit stamps but end in "DT" or "TM" as Detail Title Fields
-                    if ((viewColumnEntity.ViewField.ToUpper().EndsWith("DT") || viewColumnEntity.ViewField.ToUpper().EndsWith("TM")) && !IsAuditTabField(viewColumnEntity))
+                    // Set Any Fields that are not audit stamps but end in "Date" or "Time" as Detail Title Fields
+                    if ((viewColumnEntity.ViewField.ToUpper().EndsWith("Date") || viewColumnEntity.ViewField.ToUpper().EndsWith("Time")) && !IsAuditTabField(viewColumnEntity))
                     {
                         viewColumnEntity.IsTitleField = true;
-                    }
-
-                    var requiredField = new List<string>
-                {
-                    "NM","CNLN","CNFN","A1","A2","A3","HPC","LPC","HCY","LCY","TL","EM","OID","PID","IQT","IID","CID","ODT","SID","ST","PONO","CD","LSPR"
-                };
-                    // Set Required Fields
-                    if (requiredField.Any(x => viewColumnEntity.ViewField.ToUpper().EndsWith(x.ToUpper())))
-                    {
-                        viewColumnEntity.IsRequiredField = true;
                     }
 
                     break;
@@ -181,7 +142,7 @@ namespace GenerationWizardPlugin
         private void _SetDefaultView(AB_GenerationModuleExplorerViewEntity viewEntity,
                                       AB_GenerationModuleEntity moduleEntity)
         {
-            const string defaultViewSuffix = "LF1";
+            const string defaultViewSuffix = "Name";
 
             if (viewEntity.ViewName.ToUpper().EndsWith(defaultViewSuffix.ToUpper()) && viewEntity.Description.ToUpper().Contains("NAME"))
             {
